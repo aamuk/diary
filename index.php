2baf98f474d2f60f43299b2f13b8f79bcc4fd7e4
aamuk <aam1989@protonmail.com>
2023-03-11 22:00:56 +0000

    added tailwind cdn

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ if (isset($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="app.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
